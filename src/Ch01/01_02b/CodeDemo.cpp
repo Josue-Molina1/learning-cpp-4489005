2f7b2e224c6b75f2db0fb3398f9fe0c6b887f8a3
Josue Molina <Josue11183@byui.edu>
2025-06-16 22:22:43 -0600

    init

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,16 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "Whats your name?: " << endl;
+    cin >> name;
+    cout << name << endl;
+
+    return 0;
+}
